3c892fcb2c3e3ae5cc6110dc785c33f9b900e9e2
Abina33 <joshiabina33@gmail.com>
2019-02-18 09:13:04 +0545

    add profile to user, add account and transaction class in models.py

--- apps/ewallet/admin.py
+++ apps/ewallet/admin.py
@@ -1,26 +1,3 @@
 from django.contrib import admin
-from datetime import date
-# Register your models here.
-from .models import Profile
-
-@admin.register(Profile)
-class ProfileAdmin(admin.ModelAdmin):
-	list_display = ('address','dob','mobile','country','gender','profession')
-	list_display_links = ('address','mobile')
-	list_editable = ('dob','country')
-	list_filter = ('gender',)
-	search_fields = ('address','mobile','profession')
-
-	def get_age(self, instance): # seld ma profileadmin ko aauxa and instance ma profile ko aauxa
-		age = date.today() - instance.dob
-		return int(age.days//365.25)
-
-	get_age.short_description = 'Age'
-# admin.site.register(Profile, ProfileAdmin) #old way
 
 
-# class ProfileAdmin(admin.ModelAdmin):
-# 	list_display = ('address','dob','mobile','country','gender','profession')
-
-
-# admin.site.register(Profile)
--- apps/user_profile/admin.py
+++ apps/user_profile/admin.py
@@ -1,3 +1,48 @@
-from django.contrib import admin
 
-# Register your models here.
+from datetime import date
+
+from django.contrib import admin
+from django.contrib.auth.admin import User, UserAdmin
+
+from .models import Profile
+
+admin.site.unregister(User)
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    can_delete = False
+    verbose_name_plural = 'Profile'
+    fk_name = 'user'
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    inlines = (ProfileInline, )
+
+    def get_inline_instances(self, request, obj=None):
+        if not obj:
+            return list()
+        return super(CustomUserAdmin, self).get_inline_instances(request, obj)
+
+# admin.site.unregister(User)
+# admin.site.register(User, CustomUserAdmin)
+
+# @admin.register(Profile)
+# class ProfileAdmin(admin.ModelAdmin):
+# 	list_display = ('address','dob','mobile','country','gender','profession')
+# 	list_display_links = ('address','mobile')
+# 	list_editable = ('dob','country')
+# 	list_filter = ('gender',)
+# 	search_fields = ('address','mobile','profession')
+
+# 	def get_age(self, instance): # self ma profileadmin ko aauxa and instance ma profile ko aauxa
+# 		age = date.today() - instance.dob
+# 		return int(age.days//365.25)
+
+# 	get_age.short_description = 'Age'
+# admin.site.register(Profile, ProfileAdmin) #old way
+
+
+# class ProfileAdmin(admin.ModelAdmin):
+# 	list_display = ('address','dob','mobile','country','gender','profession')
+
+
+# admin.site.register(Profile)
