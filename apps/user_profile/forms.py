e848b232cd81d4015a7670901323c580ceeb5c36
Cybernetics Pvt. Ltd <Lenovo@SUPPORT.cybernetics.com>
2019-02-25 11:21:03 +0545

    email and mobile number validation

--- apps/user_profile/forms.py
+++ apps/user_profile/forms.py
@@ -2,19 +2,43 @@ from django import forms
 from django.contrib.auth.models import User
 
 from django.contrib.auth.forms import UserCreationForm
-from .models import Transaction
+from .models import Transaction, Profile
+from django.core.exceptions import ValidationError
 
 class CustomUserCreationForm(UserCreationForm):
-    mobile = forms.CharField(max_length=20)
-    class Meta:
-        model = User
-        fields = ('username', 'email', 'mobile')
+        mobile = forms.CharField(max_length=10)
+        class Meta:
+                model = User
+                fields = ('username', 'email', 'mobile', 'first_name', 'last_name')
+
+        def clean_email(self):
+                email = self.cleaned_data.get('email')
+                obj = User.objects.filter(email=email).exists()
+
+                if obj:
+                        raise ValidationError(
+                            ('Email already used: %(email)s'),
+                        params={'email': email},
+                                )
+                return email.strip()
+
+        def clean_mobile(self): # def clean(self) will validate all the fields of Profile, here clean_mobile_no will validate only mobile number
+                mobile = self.cleaned_data.get('mobile') # get data from form
+                obj = Profile.objects.filter(mobile=mobile).exists() # queryset, 
+
+                # print(f'Cleaned mobile_no called {obj}')
+                if obj:
+                        raise ValidationError(
+                    ('Mobile number already used: %(mobile)s'),
+                    params={'mobile': mobile},	
+                        )
+                return mobile
 
 class TransactionForm(forms.ModelForm):
-    to_mobile = forms.CharField(max_length=15)
-    
-    class Meta:
-        model = Transaction
-        fields = ['to_mobile', 'ammount']
+        to_mobile = forms.CharField(max_length=15)
+        
+        class Meta:
+                model = Transaction
+                fields = ['to_mobile', 'ammount']
 
     
--- apps/user_profile/views.py
+++ apps/user_profile/views.py
@@ -4,51 +4,51 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.exceptions import ValidationError
 
 from django.views.generic import FormView
-from django.views.generic import CreateView, DetailView
+from django.views.generic import CreateView, DetailView, UpdateView
 
 from .models import Transaction, Profile, Account
 from .forms import TransactionForm, CustomUserCreationForm
 
 
 class SignUp(CreateView):
-    form_class = CustomUserCreationForm
-    template_name = 'signup.html'
+        form_class = CustomUserCreationForm
+        template_name = 'signup.html'
 
-    success_url = '/'
+        success_url = '/'
 
-    def form_valid(self, form):
-        user = form.save()
-        mobile = form.cleaned_data.get('mobile') # cleaned data dictionary ho
-        Profile.objects.create(user=user, mobile=mobile)
-        Account.objects.create(user=user)
-        return super().form_valid(form)
+        def form_valid(self, form):
+                user = form.save()
+                mobile = form.cleaned_data.get('mobile') # cleaned data dictionary ho
+                Profile.objects.create(user=user, mobile=mobile)
+                Account.objects.create(user=user, balance=100, point=10)
+                return super().form_valid(form)
 
 class TransactionView(CreateView):
-    form_class = TransactionForm
-    template_name = 'transaction.html'
-    success_url = '/'
-    
-    def form_valid(self, form):
-        to_mobile = form.cleaned_data.get('to_mobile')
-        if self.request.user.profile.mobile ==  to_mobile:
-            raise ValidationError('balance cant transfer to self')
-        obj = form.save(commit=False)
-        obj.from_user = self.request.user
-        obj.to_user = Profile.objects.get(mobile=to_mobile).user
-        obj.save()
-        return super().form_valid(form)
+        form_class = TransactionForm
+        template_name = 'transaction.html'
+        success_url = '/'
+        
+        def form_valid(self, form):
+                to_mobile = form.cleaned_data.get('to_mobile')
+                if self.request.user.profile.mobile ==  to_mobile:
+                        raise ValidationError('balance cant transfer to self')
+                obj = form.save(commit=False)
+                obj.from_user = self.request.user
+                obj.to_user = Profile.objects.get(mobile=to_mobile).user
+                obj.save()
+                return super().form_valid(form)
 
 class UserDetailView(LoginRequiredMixin, DetailView):
-    model = User
-    template_name = 'user_detail.html'
-    # pk_url_kwarg = 'pk'
+        model = User
+        template_name = 'user_detail.html'
+        # pk_url_kwarg = 'pk'
 
-    def get_object(self, queryset=None):
-        if queryset is None:
-            queryset = self.get_queryset()
-        if self.request.user.id:
-            queryset = queryset.filter(pk=self.request.user.id) #filter logged in user
-            obj = queryset.get()
-            return obj
+        def get_object(self, queryset=None):
+                if queryset is None:
+                        queryset = self.get_queryset()
+                if self.request.user.id:
+                        queryset = queryset.filter(pk=self.request.user.id) #filter logged in user
+                        obj = queryset.get()
+                        return obj
 
 
